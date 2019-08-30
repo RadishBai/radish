e007a9cdef50ad54338189cb57625e49cf8a4a3d
baixuejiao <baixuejiao1155@dingtalk.com>
2019-08-30 11:11:44 +0800

    提交项目

---
+++ src/main/java/com/manba/radish/RadishApplication.java
@@ -0,0 +1,14 @@
+package com.manba.radish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadishApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RadishApplication.class, args);
+    }
+
+}
